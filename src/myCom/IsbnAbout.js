482862157270f5b172427db4efa1937d06f3d5f5
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-10-06 23:31:19 +0530

    isbn check function added

--- src/myCom/IsbnAbout.js
+++ src/myCom/IsbnAbout.js
@@ -1,6 +1,29 @@
 import React from 'react'
 
 export const IsbnAbout = (props) => {
+    let checkIsbn = (isbn) => {
+        let n = isbn.length;
+        if (n !== 10)
+            return false;
+        let sum = 0;
+        for (let i = 0; i < 9; i++) {
+            let digit = isbn[i] - '0';
+
+            if (0 > digit || 9 < digit)
+                return false;
+
+            sum += (digit * (10 - i));
+        }
+        let last = isbn[9];
+        if (last !== 'X' && (last < '0' || last > '9'))
+            return false;
+
+        sum += ((last === 'X') ? 10 : (last - '0'));
+        return (sum % 11 === 0);
+    }
+    let out = checkIsbn(props.isbnNumber);
+    let color = "table-" + (out ? "success" : "danger");
+    let status = out ? "Valid" : "Invalid";
     return (
         <div className="container">
             <h3 className="text-center">ISBN Details</h3>
@@ -14,10 +37,10 @@ export const IsbnAbout = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="table-light">
+                    <tr class={color}>
                         <td>{props.isbnNumber}</td>
                         <td>{props.bookName}</td>
-                        <td>Valid</td>
+                        <td>{status}</td>
                     </tr>
                 </tbody>
             </table>
